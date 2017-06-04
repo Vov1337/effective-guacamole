a31bc9891f5a2024ca9c4d3888b93b67efe499e6
Vov1337 <Vov1337@gmail.com>
2017-06-04 15:26:25 -0700

    new

--- imageprocess.m
+++ imageprocess.m
@@ -1,5 +1,5 @@
 I = imread('Koala.jpg');
-%% reducing the number of intensity levels
+%% reducing the number of intensity level
 numLevels = 10;
 Ireduced = floor(I./(256./numLevels)).*(256./numLevels);
 imshow(Ireduced)
