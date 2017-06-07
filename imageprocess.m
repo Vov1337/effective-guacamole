8c54eacf4a025bea1b2b9936e8e544177ebaf3fd
Vov1337 <Vov1337@gmail.com>
2017-06-07 19:04:45 -0400

    i figured it out

--- imageprocess.m
+++ imageprocess.m
@@ -1,24 +1,51 @@
+clear; clc; close all
 I = imread('Koala.jpg');
 I = I([200:400],[100:300],[1 2 3]);
 %% reducing the number of intensity level
 numLevels = 10;
 Ireduced = floor(I./(256./numLevels)).*(256./numLevels);
 figure(1);clf;
-  imshow(I)
-title('Not Averaged')
 
 %% performing averaging on image
-rad = 2; % number of pixels in each direction to use in average, i.e., a rad of 1 gets all neighboring pixels
-newI = zeros((size(I,1)-rad),((size(I,2)-rad)),size(I,3));
-for i = rad:(size(I,1)-rad)
-    for j = rad:(size(I,2)-rad)
-        for k = 1:size(I,3)
-            window = I([i-rad+1:i+rad],[j-rad+1:j+rad],k);
+rad = 3; % number of pixels in each direction to use in average, i.e., a rad of 1 gets all neighboring pixels
+newI = zeros((size(I,1)-rad-1),((size(I,2)-rad-1)),size(I,3));
+for i = rad+1:(size(I,1)-rad)
+    for j = rad+1:(size(I,2)-rad)
+        for k = 1:size(I,3) % for each color layer
+            window = I([i-rad:i+rad],[j-rad:j+rad],k);
             newI(i,j,k) = mean(mean(window));
         end
     
     end
 end
+newI = uint8(newI);
 figure(2);clf;
-imshow(newI);
-title('Averaged')
+subplot(2,2,1)
+imshow(newI(:,:,1));
+title('First layer')
+hold on
+subplot(2,2,2)
+imshow(newI(:,:,2))
+title('Second Layer')
+subplot(2,2,3)
+imshow(newI(:,:,3))
+title('Third Layer')
+subplot(2,2,4)
+imshow(newI)
+title('All Layers')
+
+figure(3);clf;
+subplot(2,2,1)
+imshow(I(:,:,1));
+title('First layer')
+hold on
+subplot(2,2,2)
+imshow(I(:,:,2))
+title('Second Layer')
+subplot(2,2,3)
+imshow(I(:,:,3))
+title('Third Layer')
+subplot(2,2,4)
+imshow(I)
+title('All Layers')
+
